03a9293c5964d8514c3435ae606d564afa266d4c
Matteo <zmatteo.29@gmail.com>
2023-07-12 10:58:20 +0200

    rescaling plot wilson

--- analysis/rescaling.py
+++ analysis/rescaling.py
@@ -9,38 +9,68 @@ rcParams['text.usetex'] = True
 
 mypath = "./data/"
 
-m0q = [100, 50, 20, 10, 5, 1]
-
-phi = {}
-phi_err = {}
+phi_h = {}
+phi_err_h = {}
+phi_l = {}
+phi_err_l = {}
 for direc in os.listdir(mypath):
     os.chdir("data/" + direc)
     s = direc.split("_")[1]
-    print("cutoff frac:", s)
-    phi_s = []
-    phi_s_err = []
+    #print("cutoff frac:", s)
+    phi_s = {}
+    phi_s_err = {}
     for config in sorted(os.listdir("./")):
+        g = config.split("_")[1]
         os.chdir(config)
         data = read_csv("traces.csv")
         N = len(data['phi'])
-        print("Phi:", np.average(data['phi'].to_numpy()))
-        phi_s.append(np.average(data['phi'].to_numpy()))
-        phi_s_err.append(np.std(data['phi'].to_numpy()) / np.sqrt(N-1))
+        #print("Phi:", np.average(data['phi'].to_numpy()))
+        phi_s[g] = np.average(data['phi'].to_numpy())
+        phi_s_err[g] = np.std(data['phi'].to_numpy()) / np.sqrt(N-1)
         os.chdir("..")
-    phi[s] = phi_s
-    phi_err[s] = phi_s_err
+    if direc.split("_")[2] == 'high':
+        phi_h[s] = phi_s
+        phi_err_h[s] = phi_s_err
+    elif direc.split("_")[2] == 'low':
+        phi_l[s] = phi_s
+        phi_err_l[s] = phi_s_err
+    else:
+        print("Folder name not recognised")
     os.chdir("..")
     os.chdir("..")
     print()
-    
+   
 
-'''for s in phi.keys():
-    plt.errorbar(sorted(m0q), phi[s], fmt='.-', yerr=phi_err[s], capsize=4, markersize=8, label=r'$s \, = \, ' + str(s) + '$')'''
-    #plt.plot(m0q, phi[s], '-.', label='s=' + str(s))
-plt.errorbar(m0q, phi["1.0"], fmt='.-', yerr=phi_err["1.0"], capsize=4, markersize=8, label=r'$s \, = \, ' + str(1.0) + '$')
-plt.errorbar(m0q[:3], phi["0.5"], fmt='.-', yerr=phi_err["0.5"], capsize=4, markersize=8, label=r'$s \, = \, ' + str(0.5) + '$')
-plt.xscale('log')
+for s in phi_h.keys():
+	print("Cutoff", s)
+	print("high")
+	print(phi_h[s])
+	print("low")
+	print(phi_l[s])
+
+
+for s in phi_h.keys():
+    data = []
+    for g in phi_h[s].keys():
+        #data.append((float(g), phi_h[s][g] - phi_l[s][g], np.sqrt(phi_err_h[s][g]**2 + phi_err_l[s][g]**2)))
+        #data.append((float(g), phi_h[s][g], np.sqrt(phi_err_h[s][g]**2)))
+        data.append((float(g), phi_l[s][g], np.sqrt(phi_err_l[s][g]**2)))
+    data = sorted(data, key = lambda x: x[0])
+    
+    yukawa = []
+    phi = []
+    err = []
+    for tup in data:
+        yukawa.append(tup[0])
+        phi.append(tup[1])
+        err.append(tup[2])
+    plt.errorbar(yukawa, phi, fmt='x--', yerr=err, label='s=' + str(s))
+    #phi_err[s] = [np.sqrt(h*h + l*l) for h,l in zip(phi_err_h[s], phi_err_l[s])]
+#
+# plt.errorbar(m0q, phi["1.0"], fmt='.-', yerr=phi_err["1.0"], capsize=4, markersize=8, label=r'$s \, = \, ' + str(1.0) + '$')
+#plt.errorbar(m0q[:3], phi["0.5"], fmt='.-', yerr=phi_err["0.5"], capsize=4, markersize=8, label=r'$s \, = \, ' + str(0.5) + '$')
+#plt.xscale('log')
 plt.legend()
-plt.xlabel(r'$s \, m_q$')
+plt.xlabel(r'$g / s$')
 plt.ylabel(r'$\left\langle|\phi|\right\rangle$')
 plt.savefig('rescaling.pdf')
