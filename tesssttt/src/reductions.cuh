8a590ae84672069ed318c675aa9f4e2ad36e0f3e
Matteo294 <zmatteo.29@gmail.com>
2023-09-27 15:42:50 +0200

    test

---
+++ tesssttt/src/reductions.cuh
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cmath>
+
+#include <cuda_runtime.h>
+#include <cooperative_groups.h>
+#include <cooperative_groups/reduce.h>
+
+#include "params.h"
+
+__device__ void gpuMaxAbsReduce(myType *vecA, myType *result, int size);
+__global__ void gpuTimeSlices(myType *vecA, myType *result, int size);
+__global__ void gpuMagnetisation(myType *vecA, myType *result, int size);
+
+// --------------------------------------------------------------------------------------
+__global__ void setZero_kernel(cp<double> *v, int const vol);
+__global__ void gpuTraces(double *vecA, double *result);
+__global__ void gpuTimeSlicesFermions(cp<double> *vecA, cp<double> *result, int size);
+// --------------------------------------------------------------------------------------
