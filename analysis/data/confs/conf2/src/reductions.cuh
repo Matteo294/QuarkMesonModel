605c2d68b56254e88e8ec160b53fb5dd06948932
Matteo294 <zmatteo.29@gmail.com>
2023-10-10 10:02:59 +0200

    new organisation

---
+++ analysis/data/confs/conf2/src/reductions.cuh
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cmath>
+
+#include <cuda_runtime.h>
+#include <cooperative_groups.h>
+#include <cooperative_groups/reduce.h>
+
+#include "params.h"
+
+__device__ void gpuMaxAbsReduce(myType *vecA, myType *result, int size);
+__global__ void gpuTimeSlices(myType *vecA, myType *result, int size);
+__global__ void gpuMagnetisation(myType *vecA, myType *result, int size);
+
+// --------------------------------------------------------------------------------------
+__global__ void setZero_kernel(cp<double> *v, int const vol);
+__global__ void gpuTraces(double *vecA, double *result, int const vol);
+// --------------------------------------------------------------------------------------
